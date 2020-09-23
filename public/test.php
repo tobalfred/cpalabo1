4d770d5c9c4576512c60bf1626861e45691a9da8
tobalfred <rarivohery@gmail.com>
2020-09-23 22:29:46 +0200

    Premier Deployement de l'application pour travis et heroku

---
+++ public/test.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html> 
+<html> 
+    <head> 
+        <meta charset="UTF-8"> 
+        <title>PHP Test page</title> 
+    </head> 
+    <body> 
+        <h1><?php echo("on y arrive :)")?></h1> 
+    </body> 
+</html>
